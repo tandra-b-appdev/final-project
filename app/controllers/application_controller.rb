25194c00776373b55365988a76efcd54c61a2442
Tandra Bidyananda <tandra.bidyananda@kellogg.northwestern.edu>
2019-06-13 05:43:30 +0000

    Final Project

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,3 +2,13 @@ class ApplicationController < ActionController::Base
   # protect_from_forgery with: :exception
   skip_before_action :verify_authenticity_token, raise: false
 end
+
+class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, :keys => [:username, :avatar_url])
+
+    devise_parameter_sanitizer.permit(:account_update, :keys => [:avatar_url])
+  end
+end
