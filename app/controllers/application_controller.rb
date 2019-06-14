50d6d2eb9d1bdae2fc50a326704e95d584f15d90
Tandra Bidyananda <tandra.bidyananda@kellogg.northwestern.edu>
2019-06-14 04:51:13 +0000

    Final Project

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
-class ApplicationController < ActionController::Base
+#class ApplicationController < ActionController::Base
   # protect_from_forgery with: :exception
-  skip_before_action :verify_authenticity_token, raise: false
-end
+  #skip_before_action :verify_authenticity_token, raise: false
+#end
 
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
